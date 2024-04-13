64c8fb113dd8909df489574641ec7f696c548a29
Jose Figueroa <taodejoju@gmail.com>
2024-04-13 14:29:37 -0600

    Este es mi primer aporte desde visual

---
+++ Hola Mundo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Probando desde Visual.\nAgregando al repositorio");
+Console.WriteLine("Tendre Suerte ;)");
+Console.ReadLine();
